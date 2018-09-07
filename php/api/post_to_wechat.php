9a8523c24bcf8c37da10d8e838c9a0a01cbbac5f
htroy <htroy@htroy.com>
2018-09-08 00:01:34 +0800

    Patch for 2018

--- php/api/post_to_wechat.php
+++ php/api/post_to_wechat.php
@@ -22,9 +22,9 @@ $options = array(
 write_log("\r\n\t向微信后端提交数据：" . json_encode($_POST, JSON_UNESCAPED_UNICODE) . "\r\n", __LINE__);
 if ($_POST['change'] == false) {
 	write_log("\r\n\t向微信后端添加数据，返回信息：", __LINE__);
-	$result = file_get_contents("http://wwzx.zsxyww.com/jsonpost.php", false, stream_context_create($options));
+	$result = file_get_contents("https://wwzx.htroy.com/jsonpost.php", false, stream_context_create($options));
 } else {
 	write_log("\r\n\t修改微信后端数据，返回信息：", __LINE__);
-	$result = file_get_contents("http://wwzx.zsxyww.com/jsonmodify.php", false, stream_context_create($options));
+	$result = file_get_contents("https://wwzx.htroy.com/jsonmodify.php", false, stream_context_create($options));
 }
 write_log(json_encode(json_decode($result), JSON_UNESCAPED_UNICODE) . "\r\n", 0);
